e65a8e53805bfe5f3122ad914a77a16761d3618d
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2024-03-29 17:19:24 +0100

    reboot project nextjs

--- src/components/communityImages.js
+++
@@ -1,7 +0,0 @@
-import lit from '../assets/community/lit.png'
-
-const communityImages = {
-  lit
-}
-
-export default communityImages
--- src/components/projectImages.js
+++
@@ -1,9 +0,0 @@
-import dietisy from '../assets/projects/dietisy.png'
-import guitarly from '../assets/projects/guitarly.png'
-
-const projectImages = {
-  dietisy,
-  guitarly
-}
-
-export default projectImages
